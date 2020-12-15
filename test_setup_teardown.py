6bf086c24b82d4fca03de3344a83e5e718d3ac34
Lulala <threeapples001@163.com>
2020-12-16 00:59:42 +0800

    作业1

--- test_setup_teardown.py
+++ test_setup_teardown.py
@@ -1,4 +1,33 @@
 import pytest
 
-class Test_opration():
-    
+#模块执行开始结束setup/teardown_module
+def setup_module():
+    print("setup:整个模块只执行一次开始")
+def teardown_module():
+    print("teardown:整个模块只执行一次结束")
+#非类中执行开始结束setup/teardown_function
+def setup_function():
+    print("不在类中执行用例的开始")
+def teardown_function():
+    print("不在类中执行用例的结束")
+
+def test_addthree():
+    print("test_addthree")
+def test_addfour():
+    print("tesr_addfour")
+
+class Test_pration():
+   #类中执行开始结束setup/teardown_class
+    def setup_class(self):
+        print("setup:类中用例执行开始")
+    def teardown_class(self):
+        print("setup:类中用例执行结束")
+    #类中方法执行开始结束setup/teardown_method
+    def setup_method(self):
+        print("setup:每个用例的开始")
+    def teardown_method(self):
+        print("teardown:每个用例的结束")
+    def test_add_one(self):
+        print("addOne")
+    def test_add_two(self):
+        print("addtwo")
